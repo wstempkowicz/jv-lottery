5433bc7ec2aa2fd5ceb6c5db933fab4426e13f58
wstempkowicz <80699308+wstempkowicz@users.noreply.github.com>
2026-09-05 22:46:55 +0200

    Add files via upload

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-enum Colors{
+public enum Colors{
     RED,
     GREEN,
     BLACK,
@@ -28,7 +28,7 @@ public class Ball{
         this.number = number;
     }
     public String toString(){
-        System.out.println(color + String.valueOf(number));
+        //System.out.println(color + String.valueOf(number));
         return color + String.valueOf(number);
     }
 

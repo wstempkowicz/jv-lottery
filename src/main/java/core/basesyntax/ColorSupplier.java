3936c84f53d87a80806bfbf0bc90d7652b1b9ce7
wstempkowicz <80699308+wstempkowicz@users.noreply.github.com>
2026-09-07 21:21:45 +0200

    Add files via upload

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class ColorSupplier {
     Color[] color = Color.values();
     private static final int COLOR_COUNT = color.length;
+    Random randomValueSupplier = new Random();
     public String getRandomColor() {
-        Random randomValueSupplier = new Random();
         int value = randomValueSupplier.nextInt(COLOR_COUNT);
         return color[value];
     }
